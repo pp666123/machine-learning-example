808d13a80cee7df790d648d7592e3b8372a2dd97
pp666123 <pp666123@gmail.com>
2023-08-25 00:26:50 +0800

    start ml5 example

---
+++ example_8/sketch.js
@@ -0,0 +1,32 @@
+// 定義變量
+// Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
+let classifier;
+
+// A variable to hold the image we want to classify
+let img;
+
+// 載入圖片&訓練模型
+function preload() {
+  classifier = ml5.imageClassifier("MobileNet");
+  img = loadImage("images/bird.png");
+}
+
+// p5.js顯示圖片ˋ
+function setup() {
+  createCanvas(400, 400);
+  classifier.classify(img, gotResult);
+  image(img, 0, 0);
+}
+
+// A function to run when we get any errors and the results
+function gotResult(error, results) {
+  // Display error in the console
+  if (error) {
+    console.error(error);
+  } else {
+    // The results are in an array ordered by confidence.
+    console.log(results);
+    createDiv(`Label: ${results[0].label}`);
+    createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`);
+  }
+}
